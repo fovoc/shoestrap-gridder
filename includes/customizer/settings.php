48dbe69db22ff21ae110bc2ab1a1b7ca3d482e06
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-02 07:13:57 +0200

    adding settings

---
+++ includes/customizer/settings.php
@@ -0,0 +1,19 @@
+<?php
+
+function shoestrap_blog_register_settings($wp_customize){
+
+  $settings = array();
+  $settings[] = array( 'slug'=> 'shoestrap_blog_show_text_in_lists', 'default' => 'show');
+  $settings[] = array( 'slug'=> 'shoestrap_blog_frontpage',          'default' => 'blog_list');
+
+  foreach($settings as $setting){
+    $wp_customize->add_setting( $setting['slug'], array( 'default' => $setting['default'], 'type' => 'theme_mod', 'capability' => 'edit_theme_options' ));
+  }
+  $wp_customize->add_setting( 'posts_per_page', array(
+    'default'        => '12',
+    'type'           => 'option',
+    'capability'     => 'manage_options',
+) );
+  
+}
+add_action( 'customize_register', 'shoestrap_blog_register_settings' );

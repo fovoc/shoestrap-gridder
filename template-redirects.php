9ccb7c17e3ca262d3e30b03ce7b7619d1ee3922e
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-02 07:33:58 +0200

    Added basic frontpage template

---
+++ template-redirects.php
@@ -0,0 +1,15 @@
+<?php
+  
+function shoestrap_blog_load_single_template($template) {
+  global $wp_query, $post;
+  
+  $frontpage_mode = get_theme_mod( 'shoestrap_blog_frontpage' );
+
+  // Frontpage Posts list (if selected so in the customizer)
+  if ( is_front_page() && $frontpage_mode == 'blog_list' ) {
+    return dirname(__FILE__) . '/templates/content.php';
+  }
+  
+  return $template;
+}
+add_filter('template_include', 'shoestrap_blog_load_single_template', 1, 1);

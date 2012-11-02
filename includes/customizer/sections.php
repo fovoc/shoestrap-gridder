d390f88eda7b85f38c06691da339fd06b0d1b389
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-02 07:10:40 +0200

    adding a "Blog" section

---
+++ includes/customizer/sections.php
@@ -0,0 +1,13 @@
+<?php
+
+function shoestrap_blog_register_sections($wp_customize){
+  
+  // remove default sections
+  $sections = array();
+  $sections[] = array( 'slug'=>'shoestrap_blog', 'title' => __('Blog', 'shoestrap_edd'), 'priority' => 15);
+
+  foreach($sections as $section){
+    $wp_customize->add_section( $section['slug'], array( 'title' => $section['title'], 'priority' => $section['priority']));
+  }
+}
+add_action( 'customize_register', 'shoestrap_edd_register_sections' );

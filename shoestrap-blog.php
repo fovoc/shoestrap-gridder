c22db767302f75412310fcd64f26cfb242bb32e8
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-02 06:32:00 +0200

    adding plugin info

---
+++ shoestrap-blog.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Shoestrap Blog Addon
+Plugin URI: http://bootstrap-commerce.com
+Description: This plugin adds the necessary templates and functions to the shoestrap theme
+Version: 1.0.0
+Author: Aristeides Stathopoulos
+Author URI: http://aristeides.com
+*/
+

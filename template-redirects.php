dd7637cee6742387c878abaeb044f3a0e07493ee
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-02 12:04:35 +0200

    renaming content.php to index.php

--- template-redirects.php
+++ template-redirects.php
@@ -7,7 +7,7 @@ function shoestrap_blog_load_single_template($template) {
 
   // Frontpage Posts list (if selected so in the customizer)
   if ( is_front_page() && $frontpage_mode == 'blog_list' ) {
-    return dirname(__FILE__) . '/templates/content.php';
+    return dirname(__FILE__) . '/templates/index.php';
   }
   
   return $template;

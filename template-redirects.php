a3cd23f9c53559109ddccd64474b13f022c58aac
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-02 19:09:27 +0200

    fixing template redirects and adding templates for pages and single posts

--- template-redirects.php
+++ template-redirects.php
@@ -6,10 +6,12 @@ function shoestrap_blog_load_single_template($template) {
   $frontpage_mode = get_theme_mod( 'shoestrap_blog_frontpage' );
 
   // Frontpage Posts list (if selected so in the customizer)
-  if ( is_front_page() && $frontpage_mode == 'blog_list' ) {
-    return dirname(__FILE__) . '/templates/index.php';
+  if ( !is_singular() ) {
+    return dirname(__FILE__) . '/templates/content.php';
+  } elseif ( is_page() ) {
+    return dirname(__FILE__) . '/templates/page.php';
+  } else {
+    return dirname(__FILE__) . '/templates/single.php';
   }
-  
-  return $template;
 }
 add_filter('template_include', 'shoestrap_blog_load_single_template', 1, 1);

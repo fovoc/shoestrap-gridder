1fea97ab0d7e5bbed228557108aa57901cbca8fc
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-02 11:39:32 +0200

    styling list items

---
+++ includes/customizer/output.php
@@ -0,0 +1,21 @@
+<?php
+
+function shoestrap_blog_customizer_output() {
+  
+  $background_color = get_theme_mod( 'background_color' );
+  $background_color = '#' . str_replace( '#', '', $background_color );
+  ?>
+  <style>
+    #main .entry{
+      <?php if ( shoestrap_get_brightness( $background_color ) >= 160 ) {
+        echo 'color: ' . shoestrap_adjust_brightness( $background_color, -180 ) . ';';
+        echo 'background: ' . shoestrap_adjust_brightness( $background_color, 20 ) . ';';
+      } else {
+        echo 'color: ' . shoestrap_adjust_brightness( $background_color, 180 ) . ';';
+        echo 'background: ' . shoestrap_adjust_brightness( $background_color, -20 ) . ';';
+      }?>
+    }
+  </style>
+
+<?php }
+add_action( 'wp_head', 'shoestrap_blog_customizer_output' );

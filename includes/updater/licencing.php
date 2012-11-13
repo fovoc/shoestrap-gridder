35632c644dd44e2691481cff56a87d788d9f6bbe
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-13 16:31:25 +0200

    still doesn't work

--- includes/updater/licencing.php
+++ includes/updater/licencing.php
@@ -1,7 +1,7 @@
 <?php
 
-define( 'SHOESTRAP_GRIDDER_STORE_URL', 'http://bootstrap-commerce.com/downloads' );
-define( 'SHOESTRAP_GRIDDER_ITEM_NAME', 'Shoestrap MP Addon' );
+define( 'SHOESTRAP_GR_STORE_URL', 'http://bootstrap-commerce.com/downloads' );
+define( 'SHOESTRAP_GR_ITEM_NAME', 'Shoestrap GR Addon' );
 
 if( !class_exists( 'EDD_SL_Plugin_Updater' ) ) {
   // load our custom updater
@@ -9,31 +9,31 @@ if( !class_exists( 'EDD_SL_Plugin_Updater' ) ) {
 }
 
 // retrieve our license key from the DB
-$license_key = trim( get_option( 'shoestrap_gridder_license_key' ) );
+$license_key = trim( get_option( 'shoestrap_gr_license_key' ) );
 
 // setup the updater
-$edd_updater = new EDD_SL_Plugin_Updater( SHOESTRAP_GRIDDER_STORE_URL, __FILE__, array( 
+$edd_updater = new EDD_SL_Plugin_Updater( SHOESTRAP_GR_STORE_URL, __FILE__, array( 
     'version'   => '1.00',         // current version number
     'license'   => $license_key,
-    'item_name' => SHOESTRAP_GRIDDER_ITEM_NAME,
+    'item_name' => SHOESTRAP_GR_ITEM_NAME,
     'author'    => 'Aristeides Stathopoulos'
   )
 );
 
-add_action( 'shoestrap_admin_content', 'shoestrap_gridder_licensing', 11 );
-function shoestrap_gridder_licensing() {
-  $license  = get_option( 'shoestrap_gridder_license_key' );
-  $status   = get_option( 'shoestrap_gridder_license_status' );
+add_action( 'shoestrap_admin_content', 'shoestrap_gr_licensing', 11 );
+function shoestrap_gr_licensing() {
+  $license  = get_option( 'shoestrap_gr_license_key' );
+  $status   = get_option( 'shoestrap_gr_license_status' );
   $submit_text  = __( 'Save & activate licence', 'shoestrap' );
   ?>
   <div class="postbox">
     <h3 class="hndle" style="padding: 7px 10px;"><span><?php _e( 'Shoestrap Gridder Addon Licence', 'shoestrap' ); ?></span></h3>
     <div class="inside">
       <form method="post" action="options.php">
-        <?php settings_fields('shoestrap_gridder_license'); ?>
+        <?php settings_fields('shoestrap_gr_license'); ?>
         <?php _e( 'License Key:', 'shoestrap' ); ?>
-        <input id="shoestrap_gridder_license_key" name="shoestrap_gridder_license_key" type="text" class="regular-text" value="<?php esc_attr_e( $license ); ?>" />
-        <label class="description" for="shoestrap_gridder_license_key">
+        <input id="shoestrap_gr_license_key" name="shoestrap_gr_license_key" type="text" class="regular-text" value="<?php esc_attr_e( $license ); ?>" />
+        <label class="description" for="shoestrap_gr_license_key">
             <?php _e( 'Enter your license key', 'shoestrap' ); ?>
             (
             <?php if( false !== $license ) { ?>
@@ -54,40 +54,40 @@ function shoestrap_gridder_licensing() {
 }
 
 
-add_action('admin_init', 'shoestrap_gridder_register_option');
-function shoestrap_gridder_register_option() {
+add_action('admin_init', 'shoestrap_gr_register_option');
+function shoestrap_gr_register_option() {
   // creates our settings in the options table
-  register_setting('shoestrap_gridder_license', 'shoestrap_gridder_license_key', 'shoestrap_gridder_sanitize_license' );
+  register_setting('shoestrap_gr_license', 'shoestrap_gr_license_key', 'shoestrap_gr_sanitize_license' );
 }
 
-function shoestrap_gridder_sanitize_license( $new ) {
-  $old = get_option( 'shoestrap_gridder_license_key' );
+function shoestrap_gr_sanitize_license( $new ) {
+  $old = get_option( 'shoestrap_gr_license_key' );
   if( $old && $old != $new ) {
-    delete_option( 'shoestrap_gridder_license_status' ); // new license has been entered, so must reactivate
+    delete_option( 'shoestrap_gr_license_status' ); // new license has been entered, so must reactivate
   }
   return $new;
 }
 
-add_action( 'admin_init', 'shoestrap_gridder_activate_license' );
-function shoestrap_gridder_activate_license() {
-  $license_key = trim( get_option( 'shoestrap_gridder_license_key' ) );
+add_action( 'admin_init', 'shoestrap_gr_activate_license' );
+function shoestrap_gr_activate_license() {
+  $license_key = trim( get_option( 'shoestrap_gr_license_key' ) );
   if ( strlen( $license_key ) < 7 )
     return;
 
-  if( get_option( 'shoestrap_gridder_license_status' ) == 'active' )
+  if( get_option( 'shoestrap_gr_license_status' ) == 'active' )
     return;
 
-  $license = trim( get_option( 'shoestrap_gridder_license_key' ) );
+  $license = trim( get_option( 'shoestrap_gr_license_key' ) );
 
   // data to send in our API request
   $api_params = array( 
     'edd_action'=> 'activate_license', 
     'license'   => $license, 
-    'item_name' => urlencode( SHOESTRAP_GRIDDER_ITEM_NAME ) 
+    'item_name' => urlencode( SHOESTRAP_GR_ITEM_NAME ) 
   );
   
   // Call the custom API.
-  $response = wp_remote_get( add_query_arg( $api_params, SHOESTRAP_GRIDDER_STORE_URL ) );
+  $response = wp_remote_get( add_query_arg( $api_params, SHOESTRAP_GR_STORE_URL ) );
 
   // make sure the response came back okay
   if ( is_wp_error( $response ) )
@@ -96,5 +96,5 @@ function shoestrap_gridder_activate_license() {
   // decode the license data
   $license_data = json_decode( wp_remote_retrieve_body( $response ) );
 
-  update_option( 'shoestrap_gridder_license_status', $license_data->license );
+  update_option( 'shoestrap_gr_license_status', $license_data->license );
 }

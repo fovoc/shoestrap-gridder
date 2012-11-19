4ecd7fb86dd2934e42095488ce2b5f1b67c6c33d
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-19 15:29:34 +0200

    Moving script to separate file

---
+++ assets/js/scripts.js
@@ -0,0 +1,44 @@
+jQuery(window).load(function() {
+  var $container = $('#main');
+  
+  $container.imagesLoaded( function(){
+    $container.masonry({
+
+      itemSelector: '.entry',
+      columnWidth: function( containerWidth ) {
+        return containerWidth / 12;
+      },
+      // gutterWidth: 20,
+      isResizable: true,
+      isAnimated: !Modernizr.csstransitions
+    });
+  });
+  
+  $container.imagesLoaded(function(){
+    $container.masonry({
+      itemSelector: '.entry',
+      columnWidth: 100
+    });
+  });
+
+  $container.infinitescroll({
+    navSelector  : '#post-nav',    // selector for the paged navigation
+    nextSelector : '#post-nav .previous a',  // selector for the NEXT link (to page 2)
+    itemSelector : '.entry',     // selector for all items you'll retrieve
+    loading: {
+        finishedMsg: 'No more pages to load.',
+      }
+    },
+    // trigger Masonry as a callback
+    function( newElements ) {
+      // hide new items while they are loading
+      var $newElems = $( newElements ).css({ opacity: 0 });
+      // ensure that images load before adding to masonry layout
+      $newElems.imagesLoaded(function(){
+        // show elems now they're ready
+        $newElems.animate({ opacity: 1 });
+        $container.masonry( 'appended', $newElems, true );
+      });
+    }
+  );
+});

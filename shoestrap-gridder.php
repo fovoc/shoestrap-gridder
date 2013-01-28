c9b8fbb38f600f3340592c7f27c311de2c49c9e3
Aristeides Stathopoulos <aristath@gmail.com>
2013-01-28 16:30:35 +0200

    Version 1.12

--- shoestrap-gridder.php
+++ shoestrap-gridder.php
@@ -3,7 +3,7 @@
 Plugin Name: Shoestrap Gridder Addon
 Plugin URI: http://bootstrap-commerce.com
 Description: This plugin adds the necessary templates and functions to the shoestrap theme
-Version: 1.10
+Version: 1.12
 Author: Aristeides Stathopoulos
 Author URI: http://aristeides.com
 */

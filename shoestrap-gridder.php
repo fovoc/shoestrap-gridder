9f95de94ab5084990323f3398892d7ce9ecbd6dd
Aristeides Stathopoulos <aristath@gmail.com>
2013-04-30 13:23:44 +0300

    Removing template files and using the new structure using actions instead of template redirects

--- shoestrap-gridder.php
+++ shoestrap-gridder.php
@@ -10,28 +10,21 @@ Author URI: http://aristeides.com
 
 add_image_size( 'shoestrap-gridder-grid', 350 );
 
-// Check if the Shoestrap theme is enabled.
-// Only process this plugin if the enabled theme is called shoestrap
-$shoestrap_enabled        = wp_get_theme( 'shoestrap' );
-$shoestrap_child_enabled  = wp_get_theme( 'shoestrap-child-theme' );
+require_once dirname( __FILE__ ) . '/includes/licencing.php';
+require_once dirname( __FILE__ ) . '/includes/customizer.php';
 
-require_once dirname( __FILE__ ) . '/template-redirects.php';
-
-require_once dirname( __FILE__ ) . '/includes/customizer/sections.php';
-require_once dirname( __FILE__ ) . '/includes/customizer/settings.php';
-require_once dirname( __FILE__ ) . '/includes/customizer/controls.php';
-require_once dirname( __FILE__ ) . '/includes/customizer/functions.php';
-require_once dirname( __FILE__ ) . '/includes/customizer/output.php';
-
+/*
+ * Enqueue the necessary javascript and css resources
+ */
 function shoestrap_gridder_enqueue_resources() {
 
   // Enqueue the styles
   wp_enqueue_style('shoestrap_gridder_styles', plugins_url('assets/css/style.css', __FILE__), false, null);
-  
+
   // Infinite scroll jQuery Plugin
   wp_register_script('shoestrap_gridder_infinitescroll', plugins_url( 'assets/js/jquery.infinitescroll.min.js', __FILE__ ), false, null, true);
   wp_enqueue_script('shoestrap_gridder_infinitescroll');
-	
+
 	// Masonry jQuery Plugin
   wp_register_script('shoestrap_gridder_masonry', plugins_url( 'assets/js/jquery.masonry.js', __FILE__ ), false, null, true);
   wp_enqueue_script('shoestrap_gridder_masonry');
@@ -50,6 +43,9 @@ function shoestrap_gridder_enqueue_resources() {
   wp_enqueue_script('shoestrap_gridder_script');
 }
 
+/*
+ * Only add the scripts if the user is not seing a single post, page or other custom post type.
+ */
 function shoestrap_gridder_enqueue_resources_checked() {
   if ( !is_singular() ) {
     add_action('wp_enqueue_scripts', 'shoestrap_gridder_enqueue_resources', 201);
@@ -57,5 +53,54 @@ function shoestrap_gridder_enqueue_resources_checked() {
 }
 add_action( 'wp', 'shoestrap_gridder_enqueue_resources_checked' );
 
-// Load the plugin updater
-require_once dirname( __FILE__ ) . '/includes/updater/licencing.php';
+/*
+ * The template modifications for posts on archives
+ */
+function shoestrap_gridder_template_mods() {
+  $excerpt_visibility           = get_theme_mod( 'shoestrap_gridder_show_text_in_lists' );
+  $shoestrap_gridder_post_class = shoestrap_gridder_posts_column( false );
+  $columns                      = get_theme_mod( 'shoestrap_gridder_posts_columns' );
+  $list_title_size              = get_theme_mod( 'shoestrap_gridder_list_title_size' );
+  $responsive                   = get_theme_mod( 'shoestrap_responsive' );
+
+  // Set 'normal' as the default gridder post class if it is not defined
+  if ( !isset( $shoestrap_gridder_post_class ) )
+    $shoestrap_gridder_post_class = 'normal';
+
+  // Set the layout class (fixed/responsive)
+  if ( $responsive == '0' )
+    $layout = 'fixed';
+  else
+    $layout = 'responsive';
+  ?>
+
+  <article id="post-<?php the_ID(); ?>" <?php post_class( $shoestrap_gridder_post_class . ' entry ' . $layout ); ?>>
+    <header>
+      <<?php echo $list_title_size; ?>><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></<?php echo $list_title_size; ?>>
+    </header>
+    <div class="entry-content">
+      <?php if (has_post_thumbnail()) { ?>
+        <div class="pull-left">
+          <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('shoestrap-gridder-grid'); ?></a>
+        </div>
+      <?php }?>
+
+      <?php if ( $excerpt_visibility != 'hide' )
+        the_excerpt();
+      ?>
+    </div>
+    <footer>
+      <?php the_tags('<ul class="entry-tags"><li>','</li><li>','</li></ul>'); ?>
+    </footer>
+  </article>
+  <?php
+}
+
+/*
+ * Use our custom template instead of the default
+ */
+function shoestrap_gridder_template_mods_actions() {
+  remove_action( 'shoestrap_article_content', 'shoestrap_article_content_action', 10 );
+  add_action( 'shoestrap_article_content', 'shoestrap_gridder_template_mods', 10 );
+}
+add_action( 'shoestrap_article_content', 'shoestrap_gridder_template_mods_actions', 1 );

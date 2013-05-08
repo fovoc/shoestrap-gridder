f57ed5eba603beff0ce001330ece08866e8cfc8b
Aristeides Stathopoulos <aristath@gmail.com>
2013-05-08 19:36:03 +0300

    Version 1.15

--- includes/licencing.php
+++ includes/licencing.php
@@ -13,7 +13,7 @@ $license_key = trim( get_option( 'shoestrap_gr_license_key' ) );
 
 // setup the updater
 $edd_updater = new EDD_SL_Plugin_Updater( SHOESTRAP_GR_STORE_URL, __FILE__, array( 
-    'version'   => '1.14',         // current version number
+    'version'   => '1.15',         // current version number
     'license'   => $license_key,
     'item_name' => SHOESTRAP_GR_ITEM_NAME,
     'author'    => 'Aristeides Stathopoulos'

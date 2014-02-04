4976cddd4e047bb9239a3bff97842e1ce29a223e
fovoc <fovoc.otinanai@gmail.com>
2014-02-04 22:06:18 +0200

    Github Updater credentials

--- shoestrap-gridder.php
+++ shoestrap-gridder.php
@@ -1,11 +1,13 @@
 <?php
 /*
-Plugin Name: Shoestrap 3 Gridder Addon
-Plugin URI: http://shoestrap.org/downloads/shoestrap-3-gridder-addon/
-Description: This plugin adds infinite scroll and isotope's masonry to the Shoestrap 3 theme.
-Version: 3.1.0
-Author: Aristeides Stathopoulos, Dimitris Kalliris
-Author URI: http://wpmu.io
+Plugin Name:         Shoestrap 3 Gridder Addon
+Plugin URI:          http://shoestrap.org/downloads/shoestrap-3-gridder-addon/
+Description:         This plugin adds infinite scroll and isotope's masonry to the Shoestrap 3 theme.
+Version:             3.1.0
+Author:              Aristeides Stathopoulos, Dimitris Kalliris
+Author URI:          http://wpmu.io
+GitHub Plugin URI:   https://github.com/shoestrap/shoestrap-gridder
+GitHub Access Token: 9570d6239f768f1a3cb9112db4d0b47ca84cc05b
 */
 
 if ( !defined( 'REDUX_OPT_NAME' ) )
